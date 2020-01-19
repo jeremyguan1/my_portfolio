aa7687d12720b9c578e30882b8a0eb86fd14c474
jeremyguanbusiness <43159269+jeremyguanbusiness@users.noreply.github.com>
2020-01-19 00:58:11 -0800

    good

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+const element = document.querySelector(".navigation");
+const element2 = document.querySelector(".nav-container");
+window.onscroll = function() {
+  if (window.pageYOffset > 80) {
+    element.classList.remove("nav-unSticky");
+    element.classList.add("nav-sticky");
+    element2.style.height = "3rem";
+  } else {
+    element.classList.remove("nav-sticky");
+    element.classList.add("nav-unSticky");
+    element2.style.height = "4rem";
+  }
+};
