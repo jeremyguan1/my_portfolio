64c84bc20704b174e368fd41f98ae2a280e9f15f
jeremyguanbusiness <43159269+jeremyguanbusiness@users.noreply.github.com>
2020-01-19 02:03:58 -0800

    finsihed with modal

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,31 @@
 var modal = document.getElementById("myModal");
+let modalDetail = document.querySelector(".modal-content");
+
+function markup(title) {
+  return `
+  <div class='modal-container'>
+    <div class='modal-title-wrapper'>
+      <br />
+      <h4 class="modal-title">${title}</h4>
+      <span class="close">&times;</span>
+    </div>
+    <div>
+      asdfasf
+    </div>
+  </div>
+`;
+}
 
 function popoutModal(str) {
   modal.style.display = "block";
   document.body.style.overflow = "hidden";
+  modalDetail.innerHTML = markup(str);
+  var span = document.getElementsByClassName("close")[0];
+  span.onclick = function() {
+    modal.style.display = "none";
+    document.body.style.overflow = "visible";
+  };
 }
-
-var span = document.getElementsByClassName("close")[0];
-
-span.onclick = function() {
-  modal.style.display = "none";
-  document.body.style.overflow = "visible";
-};
 
 window.onclick = function(event) {
   if (event.target == modal) {
